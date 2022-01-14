750d1117b2843ab22902b72b975edde377fde737
dvorak-hdez-mor <80235345+dvorak-hdez-mor@users.noreply.github.com>
2022-01-13 20:26:17 -0600

    Added "resize" module

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ cube.setAmplifier(amplifier)
 
 #cube.tam('cube', 1)
 
-cube.tam('cube', 2, 1, 0.3) # amplify x, y or z size coordinate of specific figure
+cube.resize('cube', 2, 1, 2) # amplify x, y or z size coordinate of specific figure
 #""" Rotate + Move 
 for i in range(181):
     oled.fill(0)
